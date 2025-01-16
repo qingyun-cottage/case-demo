27863934aec82ec2202ffbea992b57398cde2ab6
niyang <niyang@sundear.com>
2025-01-16 14:15:19 +0800

    打包发布demo

--- dist/assets/Apply-5a5d66fd.js
+++
@@ -1,1 +0,0 @@
-import{d as o,V as t,o as a,c as n,l as p}from"./index-379c0552.js";const s={class:"apply_page"},m=o({__name:"Apply",setup(c){return(r,_)=>{const e=t("router-view");return a(),n("div",s,[p(e)])}}});export{m as default};
